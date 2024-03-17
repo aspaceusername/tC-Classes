32b6c3f9c5632b985f610151172e5cf6fcc28533
Rúben Dias <aluno24705@ipt.pt>
2024-03-17 23:13:42 +0000

    model constructionInitial commit

---
+++ Classes/Classes/Models/Courses.cs
@@ -0,0 +1,9 @@
+﻿namespace Classes.Models
+{
+    public class Courses
+    {
+         public int id { get; set; }
+        public string name { get; set; }
+        public string logotype { get; set; }
+    }
+}
---
+++ Classes/Classes/Models/Students.cs
@@ -0,0 +1,9 @@
+﻿namespace Classes.Models
+{
+    public class Students
+    {
+        public int studentNumber { get; set; }
+        public decimal tuitionFees { get; set; }
+        public DateTime enrollmentDate { get; set;}
+    }
+}
